d074b7a45282edaa0b00c5f1cf5daf5de2a698d1
Tom van der Meulen <tom.vd.meulen@gmail.com>
2016-12-14 16:06:00 +0100

    Begin gemaakt aan robotbase_x

--- robotbase_x/robotbase_x.ino
+++ robotbase_x/robotbase_x.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#define SENSOR_DATA_LENGTH 8
 
+char sensor_data[SENSOR_DATA_LENGTH];  //String for the sonic sensor data
+
+void setup() {
+  Serial.begin(9600);  //Set baud rate for serial transmission
+  
+  for(int i=0; i < SENSOR_DATA_LENGTH; i++)
+    sensor_data[i] = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // If there's a command, do stuff
+  if(Serial.available() > 0) {
+    // Get twistdata
+  }
+  
+  // Print the sonic sensor data
+  if(sensor_data[0]) {
+    for(int i=0; i < SENSOR_DATA_LENGTH; i++) {
+      Serial.print(sensor_data[i]);
+      sensor_data[i] = 0;
+    }
+    
+    Serial.print("\n"); //TODO Replace with EOT?
+  }
 }
