42bfe6e45c483d010e7fbbd91d3a8215fffd94af
Stroopwafels <tom.vd.meulen@gmail.com>
2017-01-24 23:21:14 +0100

    Edited timeout test sketch

--- resources/timeout_test/timeout_test.ino
+++ resources/timeout_test/timeout_test.ino
@@ -1,15 +1,17 @@
 #include <ros.h>
 #include <std_msgs/Empty.h>
+// #include <avr/interrupt.h>
+// #include <avr/io.h>
 
 #define LED 13
 
 ros::NodeHandle  nh;
 
-unsigned long t;
+// unsigned long t;
 
 void messageCb( const std_msgs::Empty& toggle_msg){
   digitalWrite(LED, HIGH-digitalRead(LED));   // blink the led
-  unsigned long t_now = millis();
+  // unsigned long t_now = millis();
   nh.loginfo("called now");
 }
 
@@ -21,7 +23,13 @@ void setup()
   pinMode(LED, OUTPUT);
   nh.initNode();
   nh.subscribe(sub);
-  t = millis();
+  // t = millis();
+  UBRR0H = 0;//(BAUD_PRESCALE >> 8); // Load upper 8-bits of the baud rate value into the high byte of the UBRR register 
+  UBRR0L = 8; // Load lower 8-bits of the baud rate value into the low byte of the UBRR register 
+  UCSR0C |= (1 << UCSZ00) | (1 << UCSZ10); // Use 8-bit character sizes 
+  UCSR0B |= (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);   // Turn on the transmission, reception, and Receive interrupt 
+
+  interrupts();
 }
 
 void loop()
@@ -30,3 +38,6 @@ void loop()
   delay(1);
 
 }
+
+// stackoverflow.com/questions/10201590/arduino-serial-interrupts
+// http://www.engblaze.com/we-interrupt-this-program-to-bring-you-a-tutorial-on-arduino-interrupts/
