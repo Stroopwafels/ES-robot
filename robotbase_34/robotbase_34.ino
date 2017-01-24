01fdca241807725958d37aa20e90bf62f7e0ebf7
Stroopwafels <tom.vd.meulen@gmail.com>
2017-01-24 17:56:26 +0100

    In robotbase_34.ino gecommente text opgeruimd, en functies in systematische volgorde gezet.

--- robotbase_34/robotbase_34.ino
+++ robotbase_34/robotbase_34.ino
@@ -10,6 +10,7 @@
 //#include <std_msgs/Empty.h>
 #include <geometry_msgs/Twist.h>
 
+// Arduino MEGA connected pins
 #define HB_1REV   7
 #define HB_1EN    24
 #define HB_1FWD   6
@@ -20,83 +21,47 @@
 #define echoPin 22
 #define led 13
 
-double vmax = 0.16;
+// Global parameters
+double vmax_ = 0.16;
 double vl_, vr_;
 unsigned long t = millis();
 
+// ROS parameters
+ros::NodeHandle nh;
+
+//ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
+ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &messageCb );
+
+// Function declarations
 void convert2motor(geometry_msgs::Twist twist);
 //double distancefactor();
 
-ros::NodeHandle nh;
 
-void messageCb(const geometry_msgs::Twist &twist) { // const std_msgs::Empty& toggle_msg){
-  t = millis();
-  
-  convert2motor(twist);
-}
+/**
+ * Implementation
+ */
 
-//ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
+void clip_v(double *vl, double *vr) {
+  if (abs(*vl) > vmax_) {
+    *vr = *vr * vmax_/ abs(*vl);
+    *vl = *vl * vmax_/ abs(*vl);
+  }
 
-ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &messageCb );
-
-void setup()
-{ 
-  pinMode(HB_1REV, OUTPUT);
-  pinMode(HB_1EN, OUTPUT);
-  pinMode(HB_1FWD, OUTPUT);
-  pinMode(HB_2REV, OUTPUT);
-  pinMode(HB_2EN, OUTPUT);
-  pinMode(HB_2FWD, OUTPUT);
-
-  Serial.begin (9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(led, OUTPUT);
-
-  nh.initNode();
-  nh.subscribe(sub);
-}
-
-void loop()
-{  
-  nh.spinOnce();
-  double df = (double) distancefactor();
-  writespeed(vl_*df, vr_*df);
-  delay(1);
+  if (abs(*vr) > vmax_) {
+    *vl = *vl * vmax_ / abs(*vr);
+    *vr = *vr * vmax_ / abs(*vr);
+  }
 }
 
 void convert2motor(geometry_msgs::Twist twist) {
-
-  //double vmax = 0.16;
   
   double lx = twist.linear.x;
   double az = twist.angular.z; 
 
   double rotsp = az * 0.0475;
 
-//  long duration, distance;
-//  double distancefactor;
-//  digitalWrite(trigPin, LOW);
-//  delayMicroseconds(2);
-//  digitalWrite(trigPin, HIGH);
-//  delayMicroseconds(10);
-//  digitalWrite(trigPin, LOW);
-//  duration = pulseIn(echoPin, HIGH);
-//  distance = (duration/58);
-//
-//  if (distance < 30) {
-//    digitalWrite(led,HIGH);
-//    distancefactor = ((double) distance)/30;
-//  }
-//  else {
-//    digitalWrite(led,LOW);
-//    distancefactor = 1;
-//  }
-
   digitalWrite(HB_1EN, HIGH);
   digitalWrite(HB_2EN, HIGH);
-
-  double df = distancefactor();
   
   double vl = (-rotsp + lx);
   double vr = (rotsp + lx);
@@ -107,43 +72,27 @@ void convert2motor(geometry_msgs::Twist twist) {
   
   vl_ = vl;
   vr_ = vr;
-//  if (1) { //(millis()-t) < 100
-//    if (vl >= 0) {
-//      analogWrite(HB_1FWD, 255*vl/vmax);
-//      analogWrite(HB_1REV, 0);
-//    } else {
-//      analogWrite(HB_1FWD, 0);
-//      analogWrite(HB_1REV, -255*vl/vmax);
-//    }
-//    if (vr >= 0) {      
-//      analogWrite(HB_2FWD, 255*vr/vmax);
-//      analogWrite(HB_2REV, 0);
-//    } else {
-//      analogWrite(HB_2FWD, 0);
-//      analogWrite(HB_2REV, -255*vr/vmax);
-//    }
-//  } else {
-//    analogWrite(HB_1FWD, 0);
-//    analogWrite(HB_1REV, 0);
-//    analogWrite(HB_2FWD, 0);
-//    analogWrite(HB_2REV, 0);
-//  }
-
 }
 
-void clip_v(double *vl, double *vr) {
-  if (abs(*vl) > vmax) {
-    *vr = *vr * vmax/ abs(*vl);
-    *vl = *vl * vmax/ abs(*vl);
-  }
-
-  if (abs(*vr) > vmax) {
-    *vl = *vl * vmax / abs(*vr);
-    *vr = *vr * vmax / abs(*vr);
-  }
+void messageCb(const geometry_msgs::Twist &twist) { // const std_msgs::Empty& toggle_msg){
+  t = millis();
+  
+  convert2motor(twist);
 }
 
+long getdistance(){
+  long duration, distance;
 
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/58);
+  
+  return distance;
+}
 
 double distancefactor(){
   double distance = (double) getdistance();
@@ -161,35 +110,21 @@ double distancefactor(){
   return df;
 }
 
-long getdistance(){
-  long duration, distance;
-
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/58);
-  
-  return distance;
-}
-
 void writespeed(double vl, double vr) {
   if (1) { //(millis()-t) < 100
     if (vl >= 0) {
-      analogWrite(HB_1FWD, 255*vl/vmax);
+      analogWrite(HB_1FWD, 255*vl/vmax_);
       analogWrite(HB_1REV, 0);
     } else {
       analogWrite(HB_1FWD, 0);
-      analogWrite(HB_1REV, -255*vl/vmax);
+      analogWrite(HB_1REV, -255*vl/vmax_);
     }
     if (vr >= 0) {      
-      analogWrite(HB_2FWD, 255*vr/vmax);
+      analogWrite(HB_2FWD, 255*vr/vmax_);
       analogWrite(HB_2REV, 0);
     } else {
       analogWrite(HB_2FWD, 0);
-      analogWrite(HB_2REV, -255*vr/vmax);
+      analogWrite(HB_2REV, -255*vr/vmax_);
     }
   } else {
     analogWrite(HB_1FWD, 0);
@@ -199,5 +134,28 @@ void writespeed(double vl, double vr) {
   }
 }
 
-// 0.16 m/s is maximum speed
+void setup()
+{ 
+  pinMode(HB_1REV, OUTPUT);
+  pinMode(HB_1EN, OUTPUT);
+  pinMode(HB_1FWD, OUTPUT);
+  pinMode(HB_2REV, OUTPUT);
+  pinMode(HB_2EN, OUTPUT);
+  pinMode(HB_2FWD, OUTPUT);
 
+  //Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+
+  nh.initNode();
+  nh.subscribe(sub);
+}
+
+void loop()
+{  
+  nh.spinOnce();
+  double df = (double) distancefactor();
+  writespeed(vl_*df, vr_*df);
+  delay(1);
+}
